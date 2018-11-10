bd0b548eef19ca00becaf1450f1e1520b3a7ea13
dimka-zakharov <dimka.zakharov@gmail.com>
2018-11-10 20:20:07 +0300

    dragDelta fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,11 +6,13 @@ import RowResizeBar from './RowResizeBar.js';
 import {setLayout} from '../actions/main';
 import {RESIZE_BAR_SIZE} from "./RowResizeBar";
 
+const RESIZE_MIN_SIZE = 120;
+
 class Main extends Component {
+
     constructor(props) {
         super(props);
-        //this.dragDelta = null;
-        this.state = {dragInfo: null, dragRect: null};
+        this.state = {dragInfo: null, dragRect: null, dragDelta: null};
         this.mouseDown = this.mouseDown.bind(this);
         this.mouseMove = this.mouseMove.bind(this);
         this.mouseUp = this.mouseUp.bind(this);
@@ -20,9 +22,9 @@ class Main extends Component {
         let rows = [];
         this.props.layout.forEach((v, i) => {
             if (i > 0) {
-                rows.push(<RowResizeBar row={i-1} key={-i}/>);
+                rows.push(<RowResizeBar row={i - 1} key={-i}/>);
             }
-            rows.push(<Row row={v} rowIndex={i} animated={this.state.dragInfo===null} key={v.reports[0].id} total={this.props.layout.length}/>)
+            rows.push(<Row row={v} rowIndex={i} animated={this.state.dragInfo === null} key={v.reports[0].id} total={this.props.layout.length}/>)
         });
         if (this.state.dragRect !== null) {
             let rect = this.state.dragRect;
@@ -59,13 +61,17 @@ class Main extends Component {
 
             let value = event.target.attributes['data-position'].value;
             let dragInfo = value.indexOf(',') >= 0 ? {row: +value.split(',')[0], cell: +value.split(',')[1]} : {row: +value};
-            //this.dragDelta = [event.clientX - this.calculateTotalWidth(this.dragInfo.row+1, this.dragInfo.cell+1), event.clientY - this.calculateTotalHeight(this.dragInfo.row+1)];
             let dragRect = null;
             if (event.target.attributes['data-is-report']) {
                 dragInfo.report = true;
-                dragRect=this.getDragRect(event);
+                dragRect = this.getDragRect(event);
             }
-            this.setState({dragInfo: dragInfo, dragRect: dragRect});
+            let dragDelta = [
+                event.clientX - this.calculateTotalWidth(dragInfo.row, dragInfo.cell + 1) - this.screenLeft,
+                event.clientY - this.calculateTotalHeight(dragInfo.row + 1) - this.screenTop
+            ];
+            console.log(dragDelta);
+            this.setState({dragInfo, dragRect, dragDelta});
         } else {
             this.setState({dragInfo: null});
         }
@@ -120,12 +126,12 @@ class Main extends Component {
     resizeCellWidth(eventX) {
         let minWidth = this.calculateTotalWidth(this.state.dragInfo.row, this.state.dragInfo.cell);
         let maxWidth = this.calculateTotalWidth(this.state.dragInfo.row, this.state.dragInfo.cell + 2);
-        let width = eventX - this.screenLeft;
-        if (width < minWidth + RESIZE_BAR_SIZE) {
-            width = minWidth + RESIZE_BAR_SIZE;
+        let width = eventX - this.screenLeft - this.state.dragDelta[0];
+        if (width < minWidth + RESIZE_MIN_SIZE) {
+            width = minWidth + RESIZE_MIN_SIZE;
         }
-        if (width > maxWidth - RESIZE_BAR_SIZE) {
-            width = maxWidth - RESIZE_BAR_SIZE;
+        if (width > maxWidth - RESIZE_MIN_SIZE) {
+            width = maxWidth - RESIZE_MIN_SIZE;
         }
         let layout = JSON.parse(JSON.stringify(this.props.layout));
         layout[this.state.dragInfo.row].reports[this.state.dragInfo.cell].width = (width - minWidth) * 100 / this.screenWidth;
@@ -136,12 +142,12 @@ class Main extends Component {
     resizeRowHeight(eventY) {
         let minHeight = this.calculateTotalHeight(this.state.dragInfo.row);
         let maxHeight = this.calculateTotalHeight(this.state.dragInfo.row + 2);
-        let height = eventY - this.screenTop;
-        if (height < minHeight + RESIZE_BAR_SIZE) {
-            height = minHeight + RESIZE_BAR_SIZE;
+        let height = eventY - this.screenTop - this.state.dragDelta[1];
+        if (height < minHeight + RESIZE_MIN_SIZE) {
+            height = minHeight + RESIZE_MIN_SIZE;
         }
-        if (height > maxHeight - RESIZE_BAR_SIZE) {
-            height = maxHeight - RESIZE_BAR_SIZE;
+        if (height > maxHeight - RESIZE_MIN_SIZE) {
+            height = maxHeight - RESIZE_MIN_SIZE;
         }
 
         let layout = JSON.parse(JSON.stringify(this.props.layout));
